f96a653c8eaed156eee0a0b5db71d4b8c058c6cc
carswelltech <github@carswell.tech>
2022-07-25 10:13:33 -0400

    rc 1.0.0

--- zero.go
+++ zero.go
@@ -1,6 +1,10 @@
 package generics
 
-// Zero just returns the zero value of its instantiated type.
+/*
+	Zero just provides a concise way to return the zero value of the instantiated value of a type parameter.
+	Mostly useful when returning an error from a generic function or method.
+	It doesn't do any magic, but is just convenient shorthand for declaring a variable of the parametric type, and returning that variable.
+*/
 func Zero[T any]() T {
 	var r T
 	return r
